bb52e9183b381a24e66d640d91d983c9eedc32f4
shreyasprabhudev <shreyasprabhudev@gmail.com>
2025-01-06 00:07:00 -0800

    added nvmrc file

--- packages/landing/middleware.ts
+++ packages/landing/middleware.ts
@@ -1,29 +1,56 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
 
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  })
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+        },
+        remove(name: string, options: CookieOptions) {
+          response.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+        },
+      },
+    }
+  )
+
+  const { data: { session } } = await supabase.auth.getSession()
 
   // If user is not signed in and the current path is not / or /login,
   // redirect the user to /login
-  if (!session && !['/login', '/'].includes(req.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL('/login', req.url));
+  if (!session && !['/login', '/'].includes(request.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL('/login', request.url))
   }
 
   // If user is signed in and the current path is /login,
   // redirect the user to /dashboard
-  if (session && req.nextUrl.pathname === '/login') {
-    return NextResponse.redirect(new URL('/dashboard', req.url));
+  if (session && request.nextUrl.pathname === '/login') {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
   }
 
-  return res;
+  return response
 }
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-};
+}
