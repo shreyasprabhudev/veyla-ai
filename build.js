5db2cf1a1447c7802793fbf2901dc337231ee8b3
shreyasprabhudev <shreyasprabhudev@gmail.com>
2025-01-24 00:18:53 -0800

    fix: remove dashboard from Cloudflare Pages build

--- build.js
+++ build.js
@@ -10,10 +10,6 @@ fs.ensureDirSync(distDir);
 console.log('Building landing...');
 execSync('cd packages/landing && npm run build', { stdio: 'inherit' });
 
-// Build dashboard
-console.log('Building dashboard...');
-execSync('cd packages/dashboard && npm run build', { stdio: 'inherit' });
-
 // Copy landing build
 console.log('Copying landing build...');
 fs.copySync(
@@ -31,25 +27,6 @@ fs.copySync(
 const landingPublicDir = path.join(__dirname, 'packages/landing/public');
 if (fs.existsSync(landingPublicDir)) {
   fs.copySync(landingPublicDir, distDir);
-}
-
-// Copy dashboard build
-console.log('Copying dashboard build...');
-fs.copySync(
-  path.join(__dirname, 'packages/dashboard/.next/static'),
-  path.join(distDir, 'dashboard/_next/static')
-);
-
-// Copy dashboard HTML files
-fs.copySync(
-  path.join(__dirname, 'packages/dashboard/out'),
-  path.join(distDir, 'dashboard')
-);
-
-// Copy dashboard public files if they exist
-const dashboardPublicDir = path.join(__dirname, 'packages/dashboard/public');
-if (fs.existsSync(dashboardPublicDir)) {
-  fs.copySync(dashboardPublicDir, path.join(distDir, 'dashboard'));
 }
 
 console.log('Build complete!');
